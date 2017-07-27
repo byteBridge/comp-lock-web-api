byteBridge/comp-lock-web-api

commit 8d5d74bcb9b0b63d3015be07794b0bb0dd865ae0

use kebab casing for objects that will become api responses

models/users.js
@@ -139,7 +139,7 @@ function checkTimeLimits (dbUser) {
       .then(logs => {
         
         if (logs.length === 0) {
-          Object.assign(dbUser, { remainingTime: dbUser.time_limit, usedTime: '00:00:00' })
+          Object.assign(dbUser, { remaining_time: dbUser.time_limit, used_time: '00:00:00' })
           return resolve(dbUser)
         }
         
@@ -178,7 +178,7 @@ function checkTimeLimits (dbUser) {
         const usedTime = `${hours}:${minutes}:${seconds}`
         const remainingTime = `${hourLimit - hours}:${minutesLimit - minutes}:00`
 
-        Object.assign(dbUser, { remainingTime, usedTime })
+        Object.assign(dbUser, { remaining_time, used_time })
 
         resolve(dbUser)
       })

commit 538625ff8741287cf9d8c2f8a402712aefaff9af

(bugfix) assign values to the remainig_time && used_time
 -- this bug was introduced in the commit 8d5d74bcb9b0

models/users.js
@@ -184,7 +184,7 @@ function checkTimeLimits (dbUser) {
         const usedTime = `${hours}:${minutes}:${seconds}`
         const remainingTime = `${hourLimit - hours}:${minutesLimit - minutes}:00`
 
-        Object.assign(dbUser, { remaining_time, used_time })
+        Object.assign(dbUser, { remaining_time: remainingTime, used_time: usedTime })
 
         resolve(dbUser)
       })