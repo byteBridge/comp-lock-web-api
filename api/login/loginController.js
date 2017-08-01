fe574259fb31c50809b39181b9dfb034e7909c38
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-08-01 21:01:54 +0200

    implement the one object argument change in the loginController.js

--- api/login/loginController.js
+++ api/login/loginController.js
@@ -9,10 +9,11 @@ module.exports = (req, res) => {
     res.header('Cache-Control', 'no-store')
       .header('Pragma', 'no-store')
 
-    const {error, value} = validator.validate(req.body)
+    const { error, value } = validator.validate(req.body)
     if (error) return buildResponse(res, 400, { message: 'bad request', reason: error.details[0].message})
 
-    userModel.login(value.username, value.password)
+    value.computer_name = req.query.computer_name
+    userModel.login(value)
       .then(userData => {
         const { token, user } = userData
         buildResponse(res, 200, { message: 'success', token, user })
