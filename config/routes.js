05bb28a124f7ae5e4b11988741c4f997211bf9b3
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-13 18:54:08 +0200

    assign the users routes a mounting point '/users'. Also make use of the spaRoute to serve the ui

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,19 @@
 'use strict'
 
 module.exports.mount = app => {
+	const clientSpaRoute = require('../client/route')
 	const {
 		loginRoutes,
 		logoutRoutes,
 		registerRoutes,
 		secretRoutes,
+		userRoutes,
 		restoreDatabaseRoutes
 	} = require('../api/indexRoutes')
 
+	app.use('/', clientSpaRoute)
 	app.use('/secret', secretRoutes)
+	app.use('/users', userRoutes)
 	app.use('/auth/login', loginRoutes)
 	app.use('/auth/logout', logoutRoutes)
 	app.use('/auth/register', registerRoutes)
