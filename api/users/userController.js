49bbee9ee0d8e3a755465c94f49339035ee5a8f0
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-18 17:39:15 +0200

    [userController] Add function in the controller to block and unblock user

--- api/users/userController.js
+++ api/users/userController.js
@@ -22,8 +22,28 @@ function singleUserHistory (req, res) {
     .catch(err => buildResponse(res, 500, err))
 }
 
+function blockUser (req, res) {
+  const username = req.params.username
+  if (!username) return buildResponse(res, 400, { message: 'supply the username of the user you want to block'})
+
+  userModel.blockUser({ username, block: true })
+  .then(() => buildResponse(res, 200, { message: 'successfully blocked user account' }))
+  .catch(() => buildResponse(res, 500, { message: 'Something really nasty happened. Contact the developer of the software <kgparadzayi@gmail.com>'}))
+}
+
+function unblockUser (req, res) {
+  const username = req.params.username
+  if (!username) return buildResponse(res, 400, { message: 'supply the username of the user you want to block'})
+
+  userModel.blockUser({ username, block: false })
+    .then(() => buildResponse(res, 200, { message: 'successfully unblocked user account' }))
+    .catch(() => buildResponse(res, 500, { message: 'Something really nasty happened. Contact the developer of the software <kgparadzayi@gmail.com>'}))
+}
+
 module.exports = {
   allUsers,
   singleUser,
-  singleUserHistory
+  singleUserHistory,
+  blockUser,
+  unblockUser
 }
