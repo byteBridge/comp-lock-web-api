9335ac7dd12e95770499a01c5bcea7a78b44e83c
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-18 17:40:12 +0200

    [userRoutes] mount the block/unblock functions to the router to be accessible

--- api/users/userRoutes.js
+++ api/users/userRoutes.js
@@ -1,10 +1,12 @@
 'use strict'
 
 const router = require('express').Router()
-const { allUsers, singleUser, singleUserHistory } = require('./userController')
+const { allUsers, singleUser, singleUserHistory, blockUser, unblockUser } = require('./userController')
 const { authenticate } = require('../../utils/middlewareService')
 
 router.get('/:username', authenticate, singleUser)
+router.put('/:username/block', authenticate, blockUser)
+router.put('/:username/unblock', authenticate, unblockUser)
 router.get('/:username/history', authenticate, singleUserHistory)
 router.get('/', authenticate, allUsers)
 
