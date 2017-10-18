e8e5dc3ee0fccfd26abbb3cbbef9acca2d74dd37
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-18 21:44:23 +0200

    [userRoutes] mount the timelimits to the routes

--- api/users/userRoutes.js
+++ api/users/userRoutes.js
@@ -1,13 +1,32 @@
 'use strict'
 
 const router = require('express').Router()
-const { allUsers, singleUser, singleUserHistory, blockUser, unblockUser } = require('./userController')
+const {
+    allUsers,
+    singleUser,
+    singleUserHistory,
+    blockUser,
+    unblockUser,
+    getAllUserTypeTimelimits,
+    getUserTypeTimelimits,
+    createUserTypeTimelimits,
+    updateUserTypeTimelimits
+} = require('./userController')
 const { authenticate } = require('../../utils/middlewareService')
 
+// Returns a list of users
+router.get('/', authenticate, allUsers)
+
+// The timelimits stuff
+router.post('/timelimits/new', authenticate, createUserTypeTimelimits)
+router.get('/timelimits', authenticate, getAllUserTypeTimelimits)
+router.get('/timelimits/:userType', authenticate, getUserTypeTimelimits)
+router.put('/timelimits/:userType', authenticate, updateUserTypeTimelimits)
+
+// The specific user stuff
 router.get('/:username', authenticate, singleUser)
 router.put('/:username/block', authenticate, blockUser)
 router.put('/:username/unblock', authenticate, unblockUser)
 router.get('/:username/history', authenticate, singleUserHistory)
-router.get('/', authenticate, allUsers)
 
 module.exports = router
