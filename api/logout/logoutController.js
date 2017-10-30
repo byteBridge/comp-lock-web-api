a8724f63517187d6b06b82ef0d4ae366b198a8bb
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-30 17:59:47 +0200

    [api] convert the logout controller to async/await style

--- api/logout/logoutController.js
+++ api/logout/logoutController.js
@@ -2,11 +2,14 @@ const userModel = require('../../models/users')
 const { buildResponse } = require('../../utils/responseService')
 const validator = require('./logoutValidation')
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const { error, value } = validator.validate(req.body)
   if (error) return buildResponse(res, 400, { message: 'bad request', reason: error.details[0].message})
 
-  userModel.logout(value)
-    .then(() => buildResponse(res, 200, { message: 'Successfully logged out'}))
-    .catch((error) => buildResponse(res, 500, { message: 'Something aweful happend and we couldn\'t log out', error}))
+  try {
+    await userModel.logout(value)
+    buildResponse(res, 200, { message: 'Successfully logged out'})
+  } catch (error) {
+    buildResponse(res, 500, { message: 'Something aweful happend and we couldn\'t log out', error })
+  }
 }
