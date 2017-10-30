050e470ea5c2d596b820c439e202db723f8e9c39
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-30 18:03:56 +0200

    [api] convert the register controller to async/await style

--- api/register/registerController.js
+++ api/register/registerController.js
@@ -2,26 +2,19 @@ const userModel = require('../../models/users')
 const validator = require('./registerValidation')
 const { buildResponse } = require('../../utils/responseService') 
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const {error, value} = validator.validate(req.body)
   if (error) {
     return buildResponse(res, 400, { message: error.details[0].message })
   }
 
-  userModel.findOne(value.username)
-    .then(user => {
-      if (user) {
-        buildResponse(res, 422, { message: 'user already exists' })
-      } else {
-         userModel.createUser(value)
-          .then(user => {
-            buildResponse(res, 200, { message: 'successfully created user.', user })
-          })
+  try {
+    const user = await userModel.findOne(value.username)
+    if (user) return buildResponse(res, 422, { message: 'user already exists' })
 
-          .catch(error => {
-            buildResponse(res, 500, { message: 'something happened', error })
-          })
-      }
-    })
-  
+    const newUser = userModel.createUser(value)
+    buildResponse(res, 200, { message: 'successfully created user.', user: newUser })
+    } catch (error) {
+      buildResponse(res, 500, { message: 'something happened', error })
+  }
 }
