78ad81ff142a2ccee05c2f74f70467fb534600d9
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-01-07 19:45:35 +0200

    [config/routes]  only use routes from the models

--- config/routes.js
+++ config/routes.js
@@ -1,27 +1,18 @@
 'use strict'
 
-module.exports.mount = app => {
-	const {
-		loginRoutes,
-		logoutRoutes,
-		registerRoutes,
-		secretRoutes,
-		userRoutes,
-		restoreDatabaseRoutes
-	} = require('../api/indexRoutes')
+module.exports.
+mount = app => {
 	const usersRoutes = require('../models/users/user.routes')
 	const authRoutes = require('../models/auth/auth.routes')
+	const restoreDatabaseRoutes = require('../models/restore-database/restore-database.routes')
+	
 	// tomake the travis builds succeed
 	if (process.env.NODE_ENV !== 'test') {
 		const clientSpaRoute = require('../client/route')
 		app.use('/', clientSpaRoute)
 	}
+
 	app.use('/api/v1/users', usersRoutes)
 	app.use('/api/v1/auth', authRoutes)
-	app.use('/secret', secretRoutes)
-	app.use('/users', userRoutes)
-	app.use('/auth/login', loginRoutes)
-	app.use('/auth/logout', logoutRoutes)
-	app.use('/auth/register', registerRoutes)
 	app.use('/restore-database', restoreDatabaseRoutes)
 }
