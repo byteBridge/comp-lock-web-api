a3f288c9c5e032ea3e60a07e2b1fa0af9aeb94b3
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-01-07 16:14:15 +0200

    [config/routes] mount auth routes to app

--- config/routes.js
+++ config/routes.js
@@ -10,12 +10,14 @@ module.exports.mount = app => {
 		restoreDatabaseRoutes
 	} = require('../api/indexRoutes')
 	const usersRoutes = require('../models/users/user.routes')
+	const authRoutes = require('../models/auth/auth.routes')
 	// tomake the travis builds succeed
 	if (process.env.NODE_ENV !== 'test') {
 		const clientSpaRoute = require('../client/route')
 		app.use('/', clientSpaRoute)
 	}
 	app.use('/api/v1/users', usersRoutes)
+	app.use('/api/v1/auth', authRoutes)
 	app.use('/secret', secretRoutes)
 	app.use('/users', userRoutes)
 	app.use('/auth/login', loginRoutes)
