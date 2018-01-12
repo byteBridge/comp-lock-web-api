bcf5b9151bd09367c77c3fbecc2920852436899e
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-01-12 18:34:02 +0200

    [models/computers] add tests for the new computer api

---
+++ models/computers/computer.spec.e2e.js
@@ -0,0 +1,159 @@
+require('../../config/init').setEnvironment()
+process.env.NODE_ENV = 'test'
+
+const chai = require('chai')
+const should = chai.should()
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+
+const knex = require('../../database')
+const { startServer } = require('../../test')
+const { generateToken } = require('../../utils/authService')
+
+// admin token
+const token = generateToken({
+  username: 'kudakwashe',
+  type: 'administrator',
+  exp: require('moment')().add(7, 'd').unix()
+})
+
+// student token
+const studentToken =  generateToken({
+  username: 'garikai',
+  type: 'student',
+  exp: require('moment')().add(7, 'd').unix()
+})
+
+describe('register a new computer', () => {
+  let server
+  const registerUrl ='/api/v1/computers/new'
+  const getComputersUrl = '/api/v1/computers'
+
+  const computer = {
+    name: 'computer3'
+  }
+
+  before(done => {
+    startServer(runningServer => { 
+      server = runningServer
+      done()
+    })
+  })
+
+  after(done => {
+    server.close(done)
+  })
+
+  beforeEach(() => knex.migrate.rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => knex.seed.run())
+  )
+
+  afterEach(() => knex.migrate.rollback())
+  
+  describe('POST /api/v1/computers/new', () => {
+    it('should register a computer', async () => {
+      chai.request(server)
+        .post(registerUrl)
+        .set({ Authorization: token })
+        .send(computer)
+        .end((err, res) => {
+          should.not.exist(err)
+          res.redirects.length.should.eql(0)
+          res.status.should.eql(200)
+          res.type.should.eql('application/json')
+          res.body.should.contain.keys('message', 'computer')
+          res.body.message.should.eql('successfully created computer.')
+        })
+    })
+
+    it('should fail if the computer already exists', async () => {
+      // modify the computer object
+      const modifiedComputer = Object.assign({}, computer)
+      modifiedComputer.name = 'computer1'
+
+      chai.request(server)
+        .post(registerUrl)
+        .set({ Authorization: token })
+        .send(modifiedComputer)
+        .end((err, res) => {
+          should.exist(err)
+          res.redirects.length.should.eql(0)
+          res.status.should.eql(422)
+          res.type.should.eql('application/json')
+          res.body.message.should.eql('Computer already exists in the database')
+        })
+    })
+
+    it('should fail to register a computer if the client is not authenticated', async () => {
+      chai.request(server)
+        .post(registerUrl)
+        .send(computer)
+        .end((err, res) => {
+          should.exist(err)
+          res.redirects.length.should.eql(0)
+          res.status.should.eql(401)
+          res.type.should.eql('application/json')
+          res.body.should.contain.keys('message')
+          res.body.message.should.eql('No token provided.')
+        })
+    })
+
+    it('should fail to register a computer if the client is a non admin account', async () => {
+      chai.request(server)
+        .post(registerUrl)
+        .set({ Authorization: studentToken })
+        .send(computer)
+        .end((err, res) => {
+          should.exist(err)
+          res.redirects.length.should.eql(0)
+          res.status.should.eql(401)
+          res.type.should.eql('application/json')
+          res.body.should.contain.keys('message')
+          res.body.message.should.eql('Access denied. Login as administrator to continue.')
+        })
+    })
+
+    it('should fail if the name is not provided', async () => {
+      // remove the username
+      const modifiedComputer = Object.assign({}, computer)
+      modifiedComputer.name = ''
+
+      chai.request(server)
+        .post(registerUrl)
+        .set({ Authorization: token })
+        .send(modifiedComputer)
+        .end((err, res) => {
+          should.exist(err)
+          res.redirects.length.should.eql(0)
+          res.status.should.eql(400)
+          res.type.should.eql('application/json')
+          res.body.message.should.eql(`Validation errors occured`)
+          const errorMessages = [
+            '"name" is not allowed to be empty'
+          ]
+          for(let i = 0; i < errorMessages.length; i++) {
+            res.body.error.errors.find(e => e.message == errorMessages[i]).message.should.eql(errorMessages[i])
+          }
+        })
+    })
+
+  })
+
+  describe('GET /api/v1/computers', () => {
+    it('should get all the computers correctly', async () => {
+      chai.request(server)
+        .get(getComputersUrl)
+        .set({ Authorization: token })
+        .end((err, res) => {
+          should.not.exist(err)
+          res.redirects.length.should.eql(0)
+          res.status.should.eql(200)
+          res.type.should.eql('application/json')
+          res.body.computers.should.be.an('array')
+          res.body.computers[0].should.be.a('object')
+          res.body.computers[0].should.contain.keys('name', 'status', 'username', 'login_date', 'login_time', 'created_at')
+        })
+    })
+  })
+})
---
+++ models/computers/computer.spec.js
@@ -0,0 +1,99 @@
+require('../../config/init').setEnvironment()
+process.env.NODE_ENV = 'test'
+
+const chai = require('chai')
+const should = chai.should()
+const Computer =  require('./')
+const knex = require('../../database')
+
+describe('#Computer.create()', () => {
+  beforeEach(() => knex.migrate.rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => knex.seed.run())
+  )
+
+  afterEach(() => knex.migrate.rollback())
+  let newComputer = {
+    name: 'Computer3'
+  }
+
+  it('should successfully create a new computer', async () => {
+    const computerApi = new Computer()
+    const createdComputer = await computerApi.create(newComputer)
+    createdComputer.should.be.a('object')
+    createdComputer.should.contain.keys(...Object.keys(newComputer), 'created_at')
+  })
+
+  it('should fail if the computer already exists', async () => {
+    const computerApi = new Computer()
+    let computerInDb = Object.assign({}, newComputer)
+    computerInDb.name = 'computer1'
+    try {
+      const createdComputer = await computerApi.create(computerInDb)
+    } catch (error) {
+      should.exist(error)
+      error.message.should.eql('Computer already exists in the database')
+    }
+
+  })
+
+  it('should fail if the computer name is not provided', async () => {
+    let errorMessages = ['"name" is not allowed to be empty']
+    await failToCreateWithMissingValue(newComputer, 'name', ...errorMessages)
+  })
+
+  /* ======================================= MISSING KEYS =========================================== */
+  it('should fail if the name key is not provided', async () => {
+    let errorMessages = ['"name" is required']
+    await failToCreateWithMissingKey(newComputer, 'name', ...errorMessages)
+  })
+
+  it('should get all the computers correctly', async () => {
+    const computerApi = new Computer()
+    const computers = await computerApi.getAllComputers()
+    computers.should.be.an('array')
+    computers[0].should.be.a('object')
+    computers[0].should.contain.keys('name', 'status', 'username', 'login_date', 'login_time', 'created_at')
+  })
+  /**
+   * @description This function is intended to attemp to create a Computer with all keys (fields) but having some keys with no values
+   * @param {*} newComputer The Computer to be created. Ideal with all the required key fields
+   * @param {*} value The key (field) of the newComputer object that is to be made empty
+   * @param {*} errorMessages A list of error messages resulting from joi validations
+   */
+  async function failToCreateWithMissingValue (newComputer, value, ...errorMessages) {
+    const computerApi = new Computer()
+    let computerWithoutValue = Object.assign({}, newComputer)
+    computerWithoutValue[value] = ''
+    try {
+      const createdComputer = await computerApi.create(computerWithoutValue)
+    } catch (error) {
+      should.exist(error)
+      errorMessages.forEach(message => {
+        error.message.should.eql('Validation errors occured')
+        error.errors.find(e => e.message == message).message.should.eql(message)
+      })
+    }
+  }
+
+  /**
+   * @description This function is intended to attemp to create a computer with missing keys (fields)
+   * @param {*} newComputer The Computer to be created. Ideal with all the required key fields
+   * @param {*} key The key (field) of the newComputer object that is to be deleted
+   * @param {*} errorMessages A list of error messages resulting from joi validations
+   */
+  async function failToCreateWithMissingKey (newComputer, key, ...errorMessages) {
+    const computerApi = new Computer()
+    let computerWithoutKey = Object.assign({}, newComputer)
+    delete computerWithoutKey[key]
+    try {
+      const createdComputer = await computerApi.create(computerWithoutKey)
+    } catch (error) {
+      should.exist(error)
+      errorMessages.forEach(message => {
+        error.message.should.eql('Validation errors occured')
+        error.errors.find(e => e.message == message).message.should.eql(message)
+      })
+    }
+  }
+})
