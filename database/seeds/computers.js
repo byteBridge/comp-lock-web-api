747825e9b1dd1050130558351a2bccbf5637bf11
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-01-16 14:19:17 +0200

    [database/seeds] add seeds for more computers including one deactivated

--- database/seeds/computers.js
+++ database/seeds/computers.js
@@ -5,7 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('computers').insert([
-        { name: 'computer1', created_at: new Date }
+        { name: 'computer1', created_at: new Date },
+        { name: 'computer2', created_at: new Date },
+        { name: 'computer3', created_at: new Date },
+        { name: 'computer4', created_at: new Date },
+        { name: 'computer5', active: false, created_at: new Date }
       ])
     })
 }
